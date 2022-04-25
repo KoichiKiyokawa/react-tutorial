21a184e5934f273f113a882bed227015e531b744
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-04-25 19:19:13 +0900

    feat: add `Board` component

---
+++ components/Board.tsx
@@ -0,0 +1,39 @@
+// class Board extends React.Component {
+//   renderSquare(i) {
+//     return <Square value={i} />;
+//   }
+// }
+
+import { useState } from "react"
+import { Square, SquareState } from "./Square"
+
+type Props = {}
+
+export const Board: React.FC = () => {
+  const [squares, setSquares] = useState<SquareState[]>(
+    Array(9).map(() => null)
+  )
+
+  const renderSquare = (i: number) => <Square value={squares[i]} />
+
+  return (
+    <div>
+      <div className="status">{"status"}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
+}
